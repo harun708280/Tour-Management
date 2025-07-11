b4f7804b896e17a4ba3976fe96e50d0cddcba8a6
harun708280 <harun708280@gmail.com>
2025-07-11 11:55:09 +0600

    Create userInterface

---
+++ src/app/modules/user/user.interface.ts
@@ -0,0 +1,37 @@
+import { Types } from "mongoose";
+
+export enum Role {
+  SUPER_ADMIN = "SUPER_ADMIN",
+  ADMIN = "ADMIN",
+  USER = "USER",
+  GUIDE = "GUIDE",
+}
+
+// Auth Provider
+
+export interface IAUTHPROVIDER {
+  provider: string;
+  providerId: string;
+}
+
+export enum IsActive {
+  ACTIVE = "ACTIVE",
+  INACTIVE = "INACTIVE",
+  BLOCKED = "BLOCKED",
+}
+
+export interface IUser {
+  name: string;
+  email: string;
+  password?: string;
+  phone?: string;
+  picture?: string;
+  address?: string;
+  isDeleted?: string;
+  isActive?: IsActive;
+  isVerified?: string;
+  role: Role;
+  auths: IAUTHPROVIDER[];
+  bookings?: Types.ObjectId[];
+  guides?: Types.ObjectId[];
+}
