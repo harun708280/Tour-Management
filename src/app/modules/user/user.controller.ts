d456da39f644a2104c4b10d162f950b6becc0887
harun708280 <harun708280@gmail.com>
2025-07-12 14:43:44 +0600

    create User Service

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import httpStatus from "http-status-codes";
 import { User } from "./user.model";
+import { UserServices } from "./user.service";
 
 export const createUser = async (
   req: Request,
@@ -8,8 +9,7 @@ export const createUser = async (
   next: NextFunction
 ) => {
   try {
-    const { name, email } = req.body;
-    const user =await User.create({ name, email });
+    const user = await UserServices.createUserService(req.body)
     res.status(httpStatus.CREATED).json({
       message: "User Created Successfully",
       user,
